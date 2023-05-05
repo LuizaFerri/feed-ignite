ecb5f15eb42c17d70772af6cfac20d1e1cd9dc11
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2023-05-04 22:51:25 -0300

    component Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        src="https://avatars.githubusercontent.com/u/45185814?v=4"
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+            <strong>Luiza Ferri</strong>
+              <time title="11 de maio às 08:13h" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom, parabéns!!!</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
