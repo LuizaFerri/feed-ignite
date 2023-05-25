29b44eacef584021f26b7f8541854ec15f9daa97
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2023-05-24 21:18:02 -0300

    TypeScript no React

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,22 +1,38 @@
+import { useState } from "react";
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
 
-export function Comment({content, onDeleteComment}) {
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
   const handleDeleteComment = () => {
     onDeleteComment(content);
-  }
+  };
+
+  const [likeCount, setLikeCount] = useState(0);
+
+  const handleLikeComment = () => {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  };
+
   return (
     <div className={styles.comment}>
       <Avatar
         hasBorder={false}
         src="https://avatars.githubusercontent.com/u/45185814?v=4"
+        alt=""
       />
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-            <strong>Luiza Ferri</strong>
+              <strong>Luiza Ferri</strong>
               <time title="11 de maio às 08:13h" dateTime="2022-05-11 08:13:30">
                 Cerca de 1h atrás
               </time>
@@ -28,9 +44,9 @@ export function Comment({content, onDeleteComment}) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp size={20} />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
